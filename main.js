23c390215df23c0699e6716d6465012030b96038
Enrique Ramirez <nrqrmz@gmail.com>
2023-02-21 15:55:34 -0600

    finish logic for conditions

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const response = document.querySelector('#response')
 const seis = document.querySelector('#seis')
+const nueva = document.querySelector('#nueva')
 
 console.log(seis.checked)
 
 const body = document.querySelector('body')
 
 body.addEventListener('click', () => {
-  if (seis.checked) {
-    response.innerText = '6 x 6'
-  } else {
-    response.innerText = '10 x 10'
-  }
+  let size = 0
+  let brandNew = true
+
+
+  size = seis.checked ? 36 : 100
+  brandNew = nueva.checked
+
+  response.innerText = `${brandNew ? 'nueva' : 'usada'} - ${size}`
 })
