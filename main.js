ddfb60a3a25200f5cd45159ec4b99649e656e50f
Enrique Ramirez <nrqrmz@gmail.com>
2023-02-21 16:12:36 -0600

    add more and less to the counter

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const response = document.querySelector('#response')
 const seis = document.querySelector('#seis')
 const nueva = document.querySelector('#nueva')
-
-console.log(seis.checked)
-
+const minus = document.getElementById('minus')
+const plus = document.getElementById('plus')
+const counter = document.getElementById('counter')
 const body = document.querySelector('body')
+
+plus.addEventListener('click', () => {
+  counter.innerText = parseInt(counter.innerText, 0) + 1
+})
+
+minus.addEventListener('click', () => {
+  if (parseInt(counter.innerText, 0) > 0) {
+    counter.innerText = parseInt(counter.innerText, 0) - 1
+  }
+})
 
 body.addEventListener('click', () => {
   let size = 0
